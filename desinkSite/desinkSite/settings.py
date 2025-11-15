6d8080d560ffd30e520ece128ee17a740cee7ff5
FrancoArayaP <franco.araya23@inacapmail.cl>
2025-11-15 19:21:20 -0300

    integracion MySQL

--- desinkSite/desinkSite/settings.py
+++ desinkSite/desinkSite/settings.py
@@ -72,11 +72,20 @@ WSGI_APPLICATION = 'desinkSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import pymysql
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'desink_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
