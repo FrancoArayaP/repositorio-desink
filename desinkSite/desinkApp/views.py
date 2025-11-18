5e5fe910881f7109b84af012752f9bac5a9d5696
FrancoArayaP <franco.araya23@inacapmail.cl>
2025-11-18 14:50:12 -0300

    coneccion con FireBase

--- desinkSite/desinkApp/views.py
+++ desinkSite/desinkApp/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+import pyrebase
+
+# Configuración de Firebase (Python dict, no const)
+firebaseConfig = {
+    "apiKey": "AIzaSyATYekLCAB1shW9TlVdbQrjgyrE7PY-zdc",
+    "authDomain": "proyecto-titulo-bcf7e.firebaseapp.com",
+    "projectId": "proyecto-titulo-bcf7e",
+    "storageBucket": "proyecto-titulo-bcf7e.appspot.com",
+    "messagingSenderId": "566825435290",
+    "appId": "1:566825435290:web:a8f042d4d1b36c81331f23",
+    "measurementId": "G-PYYYRX57L5",
+    "databaseURL": ""  # si usas Realtime Database, agrega la URL aquí
+}
+
+# Inicializar Firebase
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+db = firebase.database()
+storage = firebase.storage()
+
+
 
 def index(request):
     return render(request, 'index.html')
